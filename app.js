684d49bb9724a8d8bbffb5825ed7ee68cd4f8faf
ADLT1304 <alexelchavodel8@gmail.com>
2022-03-31 16:13:10 -0700

    Finished Assignment

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
+//create a variable called colors in an array with green, red, rgb(133,122,200), #f15025
+//use const variable
+const colors = ["green", "red", "rgb(133,122,200)", "#f15025"];
+
+//create variable for the button using 'btn' id and color using color class
+//create onclick eventlistener for the btn
+//inside the eventlistener create variable called for a random number
+//set variable equal to a function for a random number
+//set the style property of the background equal to colors [random number variable]
+//set the textContent of color equal to colors[random number variable]
+
+const btn = document.getElementById("btn");
+
+const color = document.querySelector(".color")
+
+btn.addEventListener("click", (e) => {
+        
+    const randomNumber = getRandomNumber();
+
+    document.body.style.background = colors[randomNumber];
+
+});
+
+//create getRandomNumber function
+//return a whole number using math.floor and math.random multiplied by colors.length
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length)
+}
+
+
+
 
--- hex.js
+++ hex.js
@@ -1,1 +1,34 @@
 
+const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//#
+
+//create a variable for button
+//create a variable for the color using color class
+
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+//create eventlistener for button
+//inside function, create variable for hex color and set it equal to "#"
+//create a for loop where i is less than 6 and incrementing by 1
+//hex color variable += the hex variable [random number variable]
+//set the text content = hex color variable
+//use style property to change background to hex color
+
+btn.addEventListener("click", (e) => {
+    
+    let hexColor = "#";
+    
+    for (let i = 0; i < 6; i += 1 ) {
+        hexColor += hex[getRandomNumber()];
+    }
+
+    color.textContent = hexColor;
+    document.body.style.background = hexColor;
+});
+
+//create function for random number using math.random and math.floor multiplied by hex length
+
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * hex.length)
+}
